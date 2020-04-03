fa71b54af680013c1ef1da5af11c8b1c713731c6
NathanPB <nathan.pbombana@gmail.com>
2020-04-03 15:58:02 -0300

    Made Position a 3d vector, also created a special position type to store sides

--- src/Physical.hs
+++ src/Physical.hs
@@ -3,8 +3,12 @@ module Physical(Side, Position) where
 data Side = NORTH | SOUTH | EAST | WEST deriving (Show)
 
 data Position = Position {
-  x:: Int,
-  y:: Int,
-  z:: Int,
-  facing:: Side
+  x :: Int,
+  y :: Int,
+  z :: Int
+} deriving (Show)
+
+data SidedPosition = SidedPosition {
+  coords :: Position,
+  facing :: Side
 } deriving (Show)
